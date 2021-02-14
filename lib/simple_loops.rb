60d66139ce81e16baff851430e20f751b64ba0c4
Allen Palacios <apalacios1994@gmail.com>
2021-02-14 00:36:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,7 +3,7 @@
 def loop_message_five_times(message)
   message = ["Hello World."]
   count = 0
-  while count < 5
+  while count < 5 do
   puts message
     count += 1
   end
@@ -12,11 +12,7 @@ end
 def loop_message_n_times(message, number)
   message = "Hello Moon."
   number = 5
-  puts message
-  puts message
-  puts message
-  puts message
-  puts message
+  
   puts "#{message} to be output #{number} times"
   
   message = 'Hello Red Balloon.'
